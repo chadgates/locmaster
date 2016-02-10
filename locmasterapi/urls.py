32b8e36d1789365b96d2c5b030eb826051d45e6e
wl <wl@wassilios-lytrass-macbook-pro.local>
2016-02-10 17:34:17 +0100

    Added an API version not in use anymore  response

--- locmasterapi/urls.py
+++ locmasterapi/urls.py
@@ -2,16 +2,22 @@ from django.conf.urls import url
 from locmasterapi import views as locmasterapi_views
 
 urlpatterns = [
-    # URL pattern for the UNLocVersionListView
+    # URL pattern for the UNLocVersionList V1
     url(
         regex=r'^v1/unlocode/$',
         view=locmasterapi_views.V1_LocodeList.as_view(),
-        name='list',
+        name='v1-list',
     ),
-    # URL pattern for the UnLocVersionDetailView
+    # URL pattern for the UnLocVersionDetailView V1
     url(
         regex=r'^v1/unlocode/(?P<locode>[\w\-]+)/$',
         view=locmasterapi_views.V1_LocodeDetail.as_view(),
-        name='detail',
+        name='v1-detail',
+    ),
+    # URL pattern for the UnLocVersionList V0
+    url(
+        regex=r'^v0/unlocode/',
+        view=locmasterapi_views.v0_gone,
+        name='v0-list',
     ),
 ]
--- locmasterapi/views.py
+++ locmasterapi/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import ListAPIView
 from rest_framework.generics import RetrieveAPIView
 from unlocode.models import Locode
 from locmasterapi.serializers import LocodeSerializer
-
+from django.http import HttpResponseGone
 
 class V1_LocodeList(ListAPIView):
     queryset = Locode.objects.all()
@@ -16,3 +16,19 @@ class V1_LocodeDetail(RetrieveAPIView):
     serializer_class = LocodeSerializer
     lookup_field = 'locode'
 
+
+def v0_gone(request):
+    apiv0_gone_msg = """APIv0 was removed on January 31, 2016. Please switch to APIv1:
+                        <ul>
+                            <li>
+                                <a href="/api/v1/">APIv1 Endpoint</a>
+                            </li>
+                            <li>
+                                <a href="/apiv1_docs/">APIv1 Documentation</a>
+                            </li>
+                            <li>
+                                <a href="/apiv0_shutdown/">APIv0 shut down notice</a>
+                            </li>
+                        </ul>
+                        """
+    return HttpResponseGone(apiv0_gone_msg)
